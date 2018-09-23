639e30c62d098508ad82ebbdeb7bcc05abd8df13
Benedict Lau <ben.hy.lau@gmail.com>
2018-09-23 23:55:16 +0200

    Add jekyll site

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+# For generating course materials in PDF
 npm install -g markdown-pdf
+
+# For generating presentation slides with GitBook
 npm install -g gitbook-cli
+
+# For generating course website with Jekyll
+gem install bundler
--- package.sh
+++ package.sh
@@ -9,3 +9,9 @@ mkdir publish
 # Package assets in tar.gz and zip formats
 tar --create --gzip -f "publish/workshop-materials-${RELEASE_VERSION}.tar.gz" -C output .
 cd output; zip --recurse-paths "../publish/workshop-materials-${RELEASE_VERSION}.zip" .; cd ..
+
+# Copy hosted assets to site directory
+cp -r output site/materials
+
+# Build static website
+cd site; bundle install; bundle exec jekyll build; cd ..
