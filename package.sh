3c5051eb941df101d86f3841f229e523d2f15cab
Benedict Lau <ben.hy.lau@gmail.com>
2018-09-25 10:00:59 +0200

    Uncomment GitHub Releases packaging

--- package.sh
+++ package.sh
@@ -7,12 +7,12 @@ RELEASE_VERSION=$1
 ###################################################
 
 # Create directory for release assets ready to be published
-#rm -rf publish
-#mkdir publish
+rm -rf publish
+mkdir publish
 
 # Package assets in tar.gz and zip formats
-#tar --create --gzip -f "publish/workshop-materials-${RELEASE_VERSION}.tar.gz" -C output .
-#cd output; zip --recurse-paths "../publish/workshop-materials-${RELEASE_VERSION}.zip" .; cd ..
+tar --create --gzip -f "publish/workshop-materials-${RELEASE_VERSION}.tar.gz" -C output .
+cd output; zip --recurse-paths "../publish/workshop-materials-${RELEASE_VERSION}.zip" .; cd ..
 
 ##########################################
 # Generate hosted assets for GitHub Pages
